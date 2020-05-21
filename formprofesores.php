31164750747db18e6f5df070bced0d4b0f9b115d
jcaceresr <65689251+jcaceresr@users.noreply.github.com>
2020-05-20 22:28:47 -0400

    Create formprofesores.php

---
+++ formprofesores.php
@@ -0,0 +1,21 @@
+<?php include 'db_config.php';
+
+
+if ($_SERVER["REQUEST_METHOD"] = "POST"){
+    $idProfesor = $_POST["idProfesor"];
+    $nombre = $_POST["nombre"];
+    $apellido = $_POST["apellido"];
+    $especialidad = $_POST["especialidad"];
+    if (pg_query_params($dbconn, $sql, array($idProfesor, $nombre, $apellido, $especialidad)) !== FALSE {
+        echo "Dato ingresado correctamente";
+        $Sql = 'INSERT INTO Profesor (idProfesor, nombre, apellido, especialidad) VALUES ($1, $2, $3, $4)';
+        pg_close($dbconn);
+
+    }else{
+        echo "Hubo un error al ingresar el dato";
+        pg_close($dbconn);
+    }
+
+    
+
+?>
