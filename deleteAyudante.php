b617277ff17f3366810dedc9c06189bea7f599fb
Cupriferus <31870780+rpromerov@users.noreply.github.com>
2020-07-05 21:34:50 -0400

    Había quedado mal

--- deleteAyudante.php
+++ deleteAyudante.php
@@ -10,17 +10,7 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 </head>
 
- <!-- navbar -->
- <nav>
-        <div class="cyan darken-3 nav-wrapper">
-            <a href="index.php" class="brand-logo">Mission Control</a>
-            <ul class="right hide-on-med-and-down">
-                <li><a class="dropdown-trigger" href="#!" data-target="dropdown1">Registro<i class="material-icons right">arrow_drop_down</i></a></li>
-                <li><a href="misionDashboard.php">Misiones</a></li>
-                <li><a href="about.php">Acerca de la tarea</a></li>
-            </ul>
-        </div>
-    </nav>
+ 
 
 <?php 
 $id = $_GET["idAyudantia"];
@@ -29,37 +19,3 @@ $result = pg_query_params($dbconn, $sql, array());
 echo "<h2>Ayudante eliminado</h2>";
 echo "<a href=ayudantesDashboard.php>Volver</a>";
 ?>
-<body>
-
-
-
-<!-- footer -->
-
-<footer class="cyan darken-3 page-footer">
-    <div class="container">
-        <div class="row">
-            <div class="col l6 s12">
-                <h5 class="white-text">Mission Control</h5>
-                <p class="grey-text text-lighten-4">Tarea 2 para el ramo INF239-Bases de datos, primer semestre 2020.</p>
-            </div>
-            <div class="col l4 offset-l2 s12">
-                <h5 class="white-text">Integrantes</h5>
-                <ul>
-                    <li class="grey-text text-lighten-3">Jorge Caceres</li>
-                    <li class=" grey-text text-lighten-3">Felipe Condon</li>
-                    <li class="grey-text text-lighten-3">Roberto Romero</li>
-                </ul>
-            </div>
-        </div>
-    </div>
-    <div class="cyan darken-4 footer-copyright">
-    </div>
-</footer>
-
-<!-- Javscript dependencies -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-<script src="src/js/main.js"></script>
-
-</html>
